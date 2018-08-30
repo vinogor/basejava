c25b2fa98661e3b8ec43cf39d6045f0b3e0fb499
vinogor <andreev.aleksandr.spb@gmail.com>
2018-08-30 18:14:02 +0300

    BaseJava - HW1 - v2 (rename pointer + improve get method)

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,44 +5,42 @@ import java.util.Arrays;
  */
 public class ArrayStorage {
     private Resume[] storage = new Resume[10000];
-    private int pointerToFirstEmptyCell = 0;
+    private int pointerToFirstNull = 0;
 
     void clear() {
-        Arrays.fill(storage, 0, pointerToFirstEmptyCell, null);
-        pointerToFirstEmptyCell = 0;
+        Arrays.fill(storage, 0, pointerToFirstNull, null);
+        pointerToFirstNull = 0;
     }
 
     void save(Resume r) {
-        storage[pointerToFirstEmptyCell] = r;
-        pointerToFirstEmptyCell++;
+        storage[pointerToFirstNull] = r;
+        pointerToFirstNull++;
     }
 
     Resume get(String uuid) {
-        Resume forReturn = null;
-        for (int i = 0; i < pointerToFirstEmptyCell; i++) {
+        for (int i = 0; i < pointerToFirstNull; i++) {
             if (storage[i].uuid.equals(uuid)) {
-                forReturn = storage[i];
-                break;
+                return storage[i];
             }
         }
-        return forReturn;
+        return null;
     }
 
     void delete(String uuid) {
-        for (int i = 0; i < pointerToFirstEmptyCell; i++) {
+        for (int i = 0; i < pointerToFirstNull; i++) {
             if (storage[i].uuid.equals(uuid)) {
-                System.arraycopy(storage, i + 1, storage, i, pointerToFirstEmptyCell - i + 1);
-                pointerToFirstEmptyCell--;
+                System.arraycopy(storage, i + 1, storage, i, pointerToFirstNull - i + 1);
+                pointerToFirstNull--;
                 break;
             }
         }
     }
 
     Resume[] getAll() {
-        return Arrays.copyOfRange(storage, 0, pointerToFirstEmptyCell);
+        return Arrays.copyOfRange(storage, 0, pointerToFirstNull);
     }
 
     int size() {
-        return pointerToFirstEmptyCell;
+        return pointerToFirstNull;
     }
 }

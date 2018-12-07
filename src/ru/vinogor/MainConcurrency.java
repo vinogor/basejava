47bcae360b102ee4ce4b3c7f23d01949a4f20afe
vinogor <andreev.aleksandr.spb@gmail.com>
2018-12-07 20:24:22 +0300

    BaseJava - HW12 - add MainStreams

--- src/ru/vinogor/MainConcurrency.java
+++ src/ru/vinogor/MainConcurrency.java
@@ -1,119 +1,164 @@
 package ru.vinogor;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
 public class MainConcurrency {
-        private static final Object Lock1 = new Object();
-        private static final Object Lock2 = new Object();
-
-        public static void main(String args[]) {
-            Thread1 T1 = new Thread1();
-            Thread2 T2 = new Thread2();
-            T1.start();
-            T2.start();
-        }
-
-        private static class Thread1 extends Thread {
-            public void run() {
-                synchronized (Lock1) {
-                    System.out.println("Thread-1 - Lock1");
-                    try {
-                        Thread.sleep(10);
-                    }
-                    catch (InterruptedException e) {
-                        System.out.println("Error: " + e);
-                    }
-                    System.out.println("Thread-1 - waiting for Lock2");
-
-                    synchronized (Lock2) {
-                        System.out.println("Thread-1: Holding Lock1 and Lock2");
-                    }
-                }
-            }
-        }
-        private static class Thread2 extends Thread {
-            public void run() {
-                synchronized (Lock2) {
-                    System.out.println("Thread-2 - Lock2");
-                    try {
-                        Thread.sleep(10);
-                    }
-                    catch (InterruptedException e) {
-                        System.out.println("Error: " + e);
-                    }
-                    System.out.println("Thread-2 - waiting for Lock1");
-
-                    synchronized (Lock1) {
-                        System.out.println("Thread-2 - holding Lock1 and Lock2");
-                    }
-                }
-            }
-        }
-
-
-//    public static final int THREADS_NUMBER = 10000;
-//    private int counter;
-//    private static final Object LOCK = new Object();
+//        private static final Object Lock1 = new Object();
+//        private static final Object Lock2 = new Object();
 //
-//    public static void main(String[] args) throws InterruptedException {
-//        System.out.println(Thread.currentThread().getName());
-//
-//        Thread thread0 = new Thread() {
-//            @Override
-//            public void run() {
-//                System.out.println(getName() + ", " + getState());
-//                throw new IllegalStateException();
-//            }
-//        };
-//        thread0.start();
-//
-//        new Thread(new Runnable() {
-//
-//            @Override
-//            public void run() {
-//                System.out.println(Thread.currentThread().getName() + ", " + Thread.currentThread().getState());
-//            }
-//
-//            private void inc() {
-//                synchronized (this) {
-////                    counter++;
-//                }
-//            }
-//
-//        }).start();
-//
-//        System.out.println(thread0.getState());
-//
-//        final MainConcurrency mainConcurrency = new MainConcurrency();
-//        List<Thread> threads = new ArrayList<>(THREADS_NUMBER);
-//
-//        for (int i = 0; i < THREADS_NUMBER; i++) {
-//            Thread thread = new Thread(() -> {
-//                for (int j = 0; j < 100; j++) {
-//                    mainConcurrency.inc();
-//                }
-//            });
-//            thread.start();
-//            threads.add(thread);
+//        public static void main(String args[]) {
+//            Thread1 T1 = new Thread1();
+//            Thread2 T2 = new Thread2();
+//            T1.start();
+//            T2.start();
 //        }
 //
-//        threads.forEach(t -> {
-//            try {
-//                t.join();
-//            } catch (InterruptedException e) {
-//                e.printStackTrace();
+//        private static class Thread1 extends Thread {
+//            public void run() {
+//                synchronized (Lock1) {
+//                    System.out.println("Thread-1 - Lock1");
+//                    try {
+//                        Thread.sleep(10);
+//                    }
+//                    catch (InterruptedException e) {
+//                        System.out.println("Error: " + e);
+//                    }
+//                    System.out.println("Thread-1 - waiting for Lock2");
+//
+//                    synchronized (Lock2) {
+//                        System.out.println("Thread-1: Holding Lock1 and Lock2");
+//                    }
+//                }
 //            }
-//        });
-//        System.out.println(mainConcurrency.counter);
-//    }
+//        }
+//        private static class Thread2 extends Thread {
+//            public void run() {
+//                synchronized (Lock2) {
+//                    System.out.println("Thread-2 - Lock2");
+//                    try {
+//                        Thread.sleep(10);
+//                    }
+//                    catch (InterruptedException e) {
+//                        System.out.println("Error: " + e);
+//                    }
+//                    System.out.println("Thread-2 - waiting for Lock1");
 //
-//    private synchronized void inc() {
-////        synchronized (this) {
-////        synchronized (MainConcurrency.class) {
-//        counter++;
-////                wait();
-////                readFile
-////                ...
-////        }
-//    }
-//
+//                    synchronized (Lock1) {
+//                        System.out.println("Thread-2 - holding Lock1 and Lock2");
+//                    }
+//                }
+//            }
+//        }
 
+
+    public static final int THREADS_NUMBER = 10000;
+    private int counter;
+    private final AtomicInteger atomicCounter = new AtomicInteger();
+
+    //    private static final Object LOCK = new Object();
+//    private static final Lock lock = new ReentrantLock();
+    private static final ReentrantReadWriteLock reentrantReadWriteLock = new ReentrantReadWriteLock();
+    private static final Lock WRITE_LOCK = reentrantReadWriteLock.writeLock();
+    private static final Lock READ_LOCK = reentrantReadWriteLock.readLock();
+    private static final ThreadLocal<SimpleDateFormat> threadLocal = new ThreadLocal<SimpleDateFormat>() {
+        @Override
+        protected SimpleDateFormat initialValue() {
+            return new SimpleDateFormat();
+        }
+    };
+
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println(Thread.currentThread().getName());
+
+        Thread thread0 = new Thread() {
+            @Override
+            public void run() {
+                System.out.println(getName() + ", " + getState());
+//                throw new IllegalStateException();
+            }
+        };
+        thread0.start();
+
+        new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                System.out.println(Thread.currentThread().getName() + ", " + Thread.currentThread().getState());
+            }
+
+            private void inc() {
+                synchronized (this) {
+//                    counter++;
+                }
+            }
+
+        }).start();
+
+        System.out.println(thread0.getState());
+
+        final MainConcurrency mainConcurrency = new MainConcurrency();
+        CountDownLatch latch = new CountDownLatch(THREADS_NUMBER);
+        ExecutorService executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+//        CompletionService completionService = new ExecutorCompletionService(executorService);
+//
+//        List<Thread> threads = new ArrayList<>(THREADS_NUMBER);
+
+        for (int i = 0; i < THREADS_NUMBER; i++) {
+
+            Future<Integer> future = executorService.submit(() ->
+//            Thread thread = new Thread(() ->
+            {
+                for (int j = 0; j < 100; j++) {
+                    mainConcurrency.inc();
+                    System.out.println(threadLocal.get().format(new Date()));
+                }
+                latch.countDown();
+                return 5;
+            });
+//            thread.start();
+//            threads.add(thread);
+        }
+
+/*
+        threads.forEach(t -> {
+            try {
+                t.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+*/
+        latch.await(10, TimeUnit.SECONDS);
+        executorService.shutdown();
+//        System.out.println(mainConcurrency.counter);
+        System.out.println(mainConcurrency.atomicCounter.get());
+
+//        final String lock1 = "lock1";
+//        final String lock2 = "lock2";
+//        deadLock(lock1, lock2);
+//        deadLock(lock2, lock1);
+    }
+
+    private void inc() {
+//        synchronized (this) {
+//        synchronized (MainConcurrency.class) {
+//        WRITE_LOCK.lock();
+//        try {
+        atomicCounter.incrementAndGet();
+//            counter++;
+//        } finally {
+//            WRITE_LOCK.unlock();
+//        }
+//                wait();
+//                readFile
+//                ...
+//        }
+    }
 }
---
+++ src/ru/vinogor/MainStreams.java
@@ -0,0 +1,47 @@
+package ru.vinogor;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class MainStreams {
+    public static void main(String[] args) {
+        int[] array = {1, 4, 5, 9, 2, 5, 1, 1, 1, 1, 6, 4, 3, 5, 6};
+        System.out.println(minValue(array));
+
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(1);
+        list.add(1);
+        list.add(1);
+
+        System.out.println(list);
+        System.out.println(oddOrEven(list));
+    }
+
+    private static int minValue(int[] values) {
+        return IntStream
+                .of(values)
+                .distinct()
+                .sorted()  // убираем дубли + сортируем по возрастанию
+                .reduce((s1, s2) -> s1 * 10 + s2)
+                .getAsInt();
+    }
+
+
+    private static List<Integer> oddOrEven(List<Integer> integers) {
+
+        List<Integer> notEven = integers   // notEven - все не чётные
+                .stream()
+                .filter(o -> (o % 2) != 0)
+                .collect(Collectors.toList());
+
+        integers.removeAll(notEven); // integers - все чётные
+
+        if (notEven.size() %2 != 0) {    // то сумма чисел НЕ чётная, возвращаем чётные
+            return integers;
+        } else {
+            return notEven;
+        }
+    }
+}

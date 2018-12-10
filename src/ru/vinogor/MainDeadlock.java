81019b7235ab052e55b1f7bc1ad569f463a9ec67
vinogor <andreev.aleksandr.spb@gmail.com>
2018-12-10 11:26:24 +0300

    BaseJava - HW11 - refactor MainDeadlock

--- src/ru/vinogor/MainDeadlock.java
+++ src/ru/vinogor/MainDeadlock.java
@@ -2,39 +2,39 @@ package ru.vinogor;
 
 public class MainDeadlock {
 
-    private static final Object Lock1 = new Object();
-    private static final Object Lock2 = new Object();
+    private static final String LOCK1 = "LOCK-1";
+    private static final String LOCK2 = "LOCK-2";
 
     public static void main(String args[]) {
-        Thread1 T1 = new Thread1();
-        Thread1 T2 = new Thread1();
-        T1.start();
-        T2.start();
+
+        deadLock(LOCK1, LOCK2);
+        deadLock(LOCK2, LOCK1);
     }
 
-
-
-    private static class Thread1 extends Thread {
-        public void run() {
-            synchronized (Lock1) {
-                System.out.println("Thread-1 - Lock1");
+    private static void deadLock(String LOCK1, String LOCK2) {
+        new Thread(() -> {
+            System.out.println(Thread.currentThread().getName() + " wait " + LOCK1);
+            synchronized (LOCK1) {
+                System.out.println(Thread.currentThread().getName() + " hold " + LOCK1);
                 try {
-                    Thread.sleep(10);
+                    Thread.sleep(100);
                 } catch (InterruptedException e) {
-                    System.out.println("Error: " + e);
+                    System.out.println(Thread.currentThread().getName() + "Error: " + e);
                 }
-                System.out.println("Thread-1 - waiting for Lock2");
-
-                synchronized (Lock2) {
-                    System.out.println("Thread-1: Holding Lock1 and Lock2");
+                System.out.println(Thread.currentThread().getName() + " wait " + LOCK2);
+                synchronized (LOCK2) {
+                    System.out.println(Thread.currentThread().getName() + " hold " + LOCK2);
                 }
             }
-        }
+        }).start();
     }
+}
 
-    private static class Thread2 extends Thread {
+
+/*
+    private static class Thread1 extends Thread {
         public void run() {
-            synchronized (Lock2) {
+            synchronized (LOCK2) {
                 System.out.println("Thread-2 - Lock2");
                 try {
                     Thread.sleep(10);
@@ -43,10 +43,11 @@ public class MainDeadlock {
                 }
                 System.out.println("Thread-2 - waiting for Lock1");
 
-                synchronized (Lock1) {
+                synchronized (LOCK1) {
                     System.out.println("Thread-2 - holding Lock1 and Lock2");
                 }
             }
         }
     }
-}
+*/
+

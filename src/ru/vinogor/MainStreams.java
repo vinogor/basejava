0965fe96b6399153aeb8fe38552bbbed8041a628
vinogor <andreev.aleksandr.spb@gmail.com>
2018-12-10 11:52:15 +0300

    BaseJava - HW12 - refactor MainStreams

--- src/ru/vinogor/MainStreams.java
+++ src/ru/vinogor/MainStreams.java
@@ -7,6 +7,7 @@ import java.util.stream.IntStream;
 public class MainStreams {
     public static void main(String[] args) {
         int[] array = {1, 4, 5, 9, 2, 5, 1, 1, 1, 1, 6, 4, 3, 5, 6};
+        //     int[] array = {};
         System.out.println(minValue(array));
 
         List<Integer> list = new ArrayList<>();
@@ -20,28 +21,44 @@ public class MainStreams {
     }
 
     private static int minValue(int[] values) {
-        return IntStream
+        OptionalInt result = IntStream
                 .of(values)
                 .distinct()
                 .sorted()
-                .reduce((s1, s2) -> s1 * 10 + s2)
-                .getAsInt();
+                .reduce((s1, s2) -> s1 * 10 + s2);
+
+        if (result.isPresent()) {
+            return result.getAsInt();
+        } else {
+            return 0;
+        }
     }
 
 
     private static List<Integer> oddOrEven(List<Integer> integers) {
 
-        List<Integer> notEven = integers   // notEven - все не чётные
+//        List<Integer> notEven = integers   // notEven - все не чётные
+//                .stream()
+//                .filter(o -> (o % 2) != 0)
+//                .collect(Collectors.toList());
+//
+//        integers.removeAll(notEven); // integers - все чётные
+//
+//        if (notEven.size() % 2 != 0) {    // то сумма чисел НЕ чётная, возвращаем чётные
+//            return integers;
+//        } else {
+//            return notEven;
+//        }
+
+        Map<Boolean, List<Integer>> oddAndEven =
+                integers
                 .stream()
-                .filter(o -> (o % 2) != 0)
-                .collect(Collectors.toList());
+                .collect(Collectors.partitioningBy((p) -> p % 2 == 0));
 
-        integers.removeAll(notEven); // integers - все чётные
-
-        if (notEven.size() %2 != 0) {    // то сумма чисел НЕ чётная, возвращаем чётные
-            return integers;
+        if (oddAndEven.get(false).size() % 2 != 0 ) {
+            return oddAndEven.get(true);
         } else {
-            return notEven;
+            return oddAndEven.get(false);
         }
     }
 }

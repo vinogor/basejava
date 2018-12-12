6d258672ac119b4f1bf0848a2869267ed30f9427
vinogor <andreev.aleksandr.spb@gmail.com>
2018-12-12 14:07:25 +0300

    BaseJava - HW 12 - improve methods

--- src/ru/vinogor/MainStreams.java
+++ src/ru/vinogor/MainStreams.java
@@ -1,6 +1,8 @@
 package ru.vinogor;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -21,44 +23,20 @@ public class MainStreams {
     }
 
     private static int minValue(int[] values) {
-        OptionalInt result = IntStream
+        return IntStream
                 .of(values)
                 .distinct()
                 .sorted()
-                .reduce((s1, s2) -> s1 * 10 + s2);
-
-        if (result.isPresent()) {
-            return result.getAsInt();
-        } else {
-            return 0;
-        }
+                .reduce((s1, s2) -> s1 * 10 + s2)
+                .orElse(0);
     }
 
 
     private static List<Integer> oddOrEven(List<Integer> integers) {
-
-//        List<Integer> notEven = integers   // notEven - все не чётные
-//                .stream()
-//                .filter(o -> (o % 2) != 0)
-//                .collect(Collectors.toList());
-//
-//        integers.removeAll(notEven); // integers - все чётные
-//
-//        if (notEven.size() % 2 != 0) {    // то сумма чисел НЕ чётная, возвращаем чётные
-//            return integers;
-//        } else {
-//            return notEven;
-//        }
-
         Map<Boolean, List<Integer>> oddAndEven =
                 integers
                 .stream()
                 .collect(Collectors.partitioningBy((p) -> p % 2 == 0));
-
-        if (oddAndEven.get(false).size() % 2 != 0 ) {
-            return oddAndEven.get(true);
-        } else {
-            return oddAndEven.get(false);
-        }
+        return oddAndEven.get(oddAndEven.get(false).size() % 2 != 0);
     }
 }
